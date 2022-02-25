70c9e51ca00600ecd179f5820c8dadad5a811919
DESKTOP-6JOKMHA\usuario <healfer@yahoo.es>
2022-02-25 20:02:10 +0100

    hecho data context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import HomePage from './pages/Home';
 import './App.css';
 import React from 'react';
+import DataProvider from './components/context/dataProvider';
 
 
 function App() {
   return (
+
+    <DataProvider>
     <React.Fragment>
     
       <HomePage></HomePage>
 
       </React.Fragment>
+      </DataProvider>
   );
 }
 
